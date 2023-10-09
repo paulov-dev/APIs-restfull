b018c69bfe5846d537fa8b0a00882f9b7b2cec52
paulov-dev <paulovmmf@gmail.com>
2023-10-09 17:12:26 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const consign = require('consign')
+const middlewares = require('./middlewares')
+
+const PORT = 3000
+const app = express()
+
+app.set('json spaces', 4)
+
+consign()
+    .include('models')
+    .then('middlewares.js')
+    .then('routes')
+    .then('boot.js')
+    .into(app)
